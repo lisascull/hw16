49c8ce13e99dac36e6fb6b2b626fe51cc71c0fc7
lisascull <lisa29082005@gmail.com>
2023-05-27 15:31:37 +0300

    16

---
+++ main.py
@@ -0,0 +1,29 @@
+#1
+import pickle
+files = [
+    {'ключ1': 'значення1', 'ключ2': 'значення2'},
+    {'ключ1': 'значення3', 'ключ2': 'значення4'},
+    {'ключ1': 'значення5', 'ключ2': 'значення6'}
+]
+with open('data.pkl', 'wb') as file:
+    pickle.dump(files, file)
+#2
+import json
+
+A = {'key': 1}
+B = {'key1': 2}
+C = {}
+
+for key, unite in A.items():
+    C[key] = unite
+
+for key, unite in B.items():
+    if key in C:
+        if isinstance(C[key], list):
+            C[key].append(unite)
+        else:
+            C[key] = [C[key], unite]
+    else:
+        C[key] = unite
+with open('result.json', 'w') as file:
+    json.dump(C, file)
